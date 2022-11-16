84da0ed68c5a37921c1e832c4445969d661bda21
murtaza heyderi <107448355+Saintmori@users.noreply.github.com>
2022-11-16 11:06:08 -0600

    Update ec2.tf

--- session-2/ec2.tf
+++ session-2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web_instance" {
   ami           = "ami-026b57f3c383c2eec"
   instance_type = "t2.micro"
   tags = {
-    Name        = "first"
+    Name        = "second"
     Environment = "dev"
   }
 }
